f567b189f619a22d91abbbbd9fbedc08a9f4f201
lamoirex <ilya-burmistrov-2007@mail.ru>
2025-09-16 13:35:50 +0300

    modify calculator.py

--- calculator.py
+++ calculator.py
@@ -2,3 +2,4 @@ first_number = int(input("Введите первое целое число\n"))
 second_number = int(input("Введите второе целое число\n"))
 
 print(f'Разность введенных чисел: {first_number - second_number}')
+print("Calculated!")
