f38867aeaf56f9d24a10b9460f630ce88bb95576
lamoirex <ilya-burmistrov-2007@mail.ru>
2025-09-16 10:07:00 +0300

    modify calculator.py(added the summation of numbers)

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
-first_number = int(input("Введите первое целое число"))
-second_number = int(input("Введите второе целое число"))
+first_number = int(input("Введите первое целое число\n"))
+second_number = int(input("Введите второе целое число\n"))
 
+print(f'Сумма введенных чисел: {first_number + second_number}')
